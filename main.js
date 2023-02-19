411b61ffc8cc1fc53e160b50a81e388f7fa98035
Abdullahi Mohammed <odanwu211@gmail.com>
2023-02-20 00:00:18 +0100

    update

---
+++ main.js
@@ -0,0 +1,3 @@
+"strict";const popUpEl = document.getElementById("pop_up"); window.onload=setTimeout(function() { popUpEl.style.display="flex";setTimeout(function() { 
+popUpEl.style.display="none"},10000);},-500)
+
